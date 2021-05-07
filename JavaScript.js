e815a8bc3422a148f32cea70f7e64958284cdf6d
Muhammad Mappanyompa <defcoolz@gmail.com>
2021-05-07 15:29:32 +0900

    Introduction part to Initialize variables in JavaScript

---
+++ JavaScript.js
@@ -0,0 +1,20 @@
+//Creating Comment
+/* creating comment*/
+
+//declaring variable
+var myName;
+var myVar, x, y;
+myVar = 5;
+x=1;
+y=2;
+
+//declaring variables 2
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
