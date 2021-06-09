a419642873e43936f91dd6c4def4d56b41bc5842
Muhammad Mappanyompa <defcoolz@gmail.com>
2021-06-09 16:01:19 +0900

    initialization var, let, const

--- JavaScript_Project/main.js
+++ JavaScript_Project/main.js
@@ -1,7 +1,19 @@
 console.log('Hello World');
 console.error('This is an Error');
+console.warn("This is a Warning");
 /*console/
 alert
 clear
-
 */
+
+
+//Variables in JavaScript
+//var , let , const
+
+let age = 30; //let
+age = 31; //let variable can be continued
+console.log(age);
+
+const a = 1;
+//a = 2;// output will be error cause duplicate the initialization of the variable
+console.log(a);
