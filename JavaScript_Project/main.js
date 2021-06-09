b895fe2bf3368c2da0180625cc9e479babeb606c
Muhammad Mappanyompa <defcoolz@gmail.com>
2021-06-09 18:01:15 +0900

    Array tutorial

--- JavaScript_Project/main.js
+++ JavaScript_Project/main.js
@@ -56,3 +56,28 @@ const s = 'Hello World';
 console.log(s.length); // length of the string
 console.log(s.toUpperCase()); // using uppercase method
 console.log(s.toLowerCase());
+console.log(s.substring(0,5)); //Substring (specified string)
+console.log(s.split(''));// Split each string to see detail of each string
+
+const t = 'technology , computers , it , code';
+console.log(t.split(' , ')); // split each string
+
+//Arrays : variables that hold multiple values
+
+const num = new Array(1,2,3,4,5); //new Array() : Constructor
+
+//const fruits = ['apples', 'oranges','pears',10,true];
+
+const fruits = ['apples', 'oranges','pears'];
+
+fruits [3] = 'grapes';
+fruits.push('mangos'); // adding the value after
+fruits.unshift('strawberries'); //adding the value before
+fruits.pop(); //remove the last value
+
+console.log(Array.isArray('hello'));
+console.log(Array.isArray(fruits));
+console.log(fruits.indexOf('oranges'));
+console.log(fruits[1]); // [] : read the value from the array
+console.log(fruits); // adding more value to array 
+
