221b76599f595bf5a5955c4f2a1de82b96888dd6
Muhammad Mappanyompa <defcoolz@gmail.com>
2021-08-21 01:05:04 +0900

    object in js

---
+++ freecodecamp/objects.js
@@ -0,0 +1,23 @@
+//Example
+var ourDog = { //variable
+//property and value
+"name":"doggo", // using coma at the next property
+"legs": 4,
+"tails":1,
+"friends" : ["Human"]
+}; //close the bracket with ;
+
+var dogHum = ourDog.name;
+console.log(dogHum);
+
+//using array bracket
+var testObj = {
+    "an entree": "hamburger",
+    "my side": "veggies",
+    "the drink": "water"
+  };
+  
+  // Only change code below this line
+  //access using array
+  var entreeValue = testObj["an entree"];   // Change this line
+  var drinkValue = testObj["the drink"];    // Change this line
