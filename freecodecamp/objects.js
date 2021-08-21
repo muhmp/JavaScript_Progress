ea3cc1428551cd0026ec8d6a72017c41565b7c44
Muhammad Mappanyompa <defcoolz@gmail.com>
2021-08-21 15:12:47 +0900

    parameter object searching

--- freecodecamp/objects.js
+++ freecodecamp/objects.js
@@ -20,4 +20,73 @@ var testObj = {
   // Only change code below this line
   //access using array
   var entreeValue = testObj["an entree"];   // Change this line
-  var drinkValue = testObj["the drink"];    // Change this line
+  var drinkValue = testObj["the drink"];    //
+
+
+  // Setup
+var testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line
+
+var playerNumber = 16;       // Change this line
+var player = testObj[playerNumber];   // called the object using bracket
+
+//adding new properties
+var myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+ 
+ myDog.bark = "woof !"; //adding object to the previous object
+ 
+ //delete object
+ //delete myDog.bark;
+ console.log(myDog.bark);
+
+
+ // Setup
+function phoneticLookup(val) {
+  var result = "";
+
+  // Only change code below this line
+  var lookup = {
+    "alpha": "Adams",
+    "bravo": "Boston",
+    "charlie": "Chicago",
+    "delta" : "Denver",
+    "echo":"Easy",
+    "foxtrot": "frank"
+  };
+  result = lookup[val];
+  // Only change code above this line
+  return result;
+}
+
+console.log(phoneticLookup("charlie"));
+
+
+//testing object (hasOwnProperty)
+function checkObj(obj, checkProp) {
+  // Only change code below this line
+   if (obj.hasOwnProperty(checkProp)) {
+    return obj[checkProp];
+  } else {
+    return "Not Found";
+  }
+  // Only change code above this line
+}
+
+
+function checkObj(obj, checkprop){
+  if(obj.hasOwnProperty(checkprop)){
+    return obj.checkprop; //checking the checkprop parameter
+  }else{
+    return "not found"
+  }
+}
