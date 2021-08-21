cabd41681d13cc8bc111ebd6de6c451a01ce788b
Muhammad Mappanyompa <defcoolz@gmail.com>
2021-08-21 17:34:51 +0900

    completing object tutorial

--- freecodecamp/objects.js
+++ freecodecamp/objects.js
@@ -90,3 +90,135 @@ function checkObj(obj, checkprop){
     return "not found"
   }
 }
+
+
+//complex object
+var myMusic = [
+  { "artist": "Billy Joel","title": "Piano Man","release_year": 1973,"formats": ["CD","8T","LP" ], "gold": true}
+];
+
+
+//another format
+var ourMusic = [
+  {
+    "artist": "Daft Punk",
+    "title": "Homework",
+    "release_year": 1997,
+    "formats": [ 
+      "CD", 
+      "Cassette", 
+      "LP"
+    ],
+    "gold": true
+  }
+];
+
+
+var modernMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  }, //look for the open and close bracket
+  {
+  //Add record (look after the coma)
+    "artist": "LP",
+    "title": "In the End",
+    "release_year": 2000,
+    "formats": [
+      "YouTube Video",
+      "CD"
+      ]
+  }
+];
+
+//nested object
+var myStorage = {
+  "car": { //nested
+    "inside":{ //inside
+      "glove box" : "maps",
+      "passanger seat": "Chairs"
+    },
+    "outside":{ //nested
+      "color":"black"
+    }
+  }
+};
+
+var accessStorage = myStorage.car.inside["glove box"];
+//print
+console.log(accessStorage);
+
+//nested array
+var myPlants = [ //(array)
+  { //obj 1
+    type: "flowers",
+    list: [ //key value (array)
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  { //obj 2
+    type: "trees",
+    list: [ //key value  (array)
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
+
+var secondTree = myPlants[1].list[1]; //access the second tree
+console.log(secondTree);
+
+//ID, Property, Value
+//2458:{//ID
+// "album": "single" //Property:value
+//} 
+
+
+
+// Setup
+var recordCollection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name']
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: []
+  },
+  5439: {
+    albumTitle: 'ABBA Gold'
+  }
+};
+
+// Update records
+function updateRecords(records, id, prop, value) {
+  if (value === ""){ // if the value is strict equal
+    delete records[id][prop];
+
+  } else if (prop === "tracks"){ //if property is strict equal
+    records[id][prop] = records[id][prop] || [];
+    records[id][prop].push(value); 
+  } else {
+    records[id][prop] = value;
+  }
+  
+  return records;
+}
+//calling the function from recordCollection information, id , property and value using update records function
+console.log(updateRecords(recordCollection, 5439, 'artist', 'ABBA'));
