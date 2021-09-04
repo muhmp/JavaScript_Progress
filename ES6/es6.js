00674eaf60b30effe1a8530f4c9c41fb2676b264
muhmp <defcoolz@gmail.com>
2021-09-04 20:04:37 +0900

    mutate array

--- ES6/es6.js
+++ ES6/es6.js
@@ -9,3 +9,39 @@ function catTalk(){
 }
 
 console.log(catTalk(quote));
+
+
+//let:  limited to that block, statement, or expression.
+
+function checkScope() {
+    let i = 'function scope';
+    if (true) {
+      i = 'block scope';
+      console.log('Block scope i is: ', i);
+    }
+    console.log('Function scope i is: ', i);
+    return 'function scope';
+  }
+  
+
+//const CAPITAL LETTER
+function printManyTimes(str){
+    const SENTENCE = str + " is cool!";
+    for (let i = 0 ; i < str.length; i+=2){
+        console.log(SENTENCE);
+    }
+}
+
+    printManyTimes("Mamet");
+
+
+
+//mutate array
+const s=[5,7,2]; //we wanted to ignore this
+function editInPlace(){
+    //mutation
+    s[0] = 2;
+    s[1] = 5;
+    s[2] = 7;
+}
+    editInPlace();
