60079090699a0d4306c66123d2d2a4f2bf91884e
muhmp <defcoolz@gmail.com>
2021-09-04 19:29:04 +0900

    es6 file

---
+++ ES6/es6.js
@@ -0,0 +1,11 @@
+//using let keyword
+let catName;
+let quote;
+//function
+function catTalk(){
+    "use strict";
+    catName = "Meow";
+    quote = catName + "says Meow!";
+}
+
+console.log(catTalk(quote));
