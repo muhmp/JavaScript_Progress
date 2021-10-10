d81067e0c17a0930a863af9eec2a12271edfe8b6
muhmp <defcoolz@gmail.com>
2021-10-10 18:58:33 +0900

    add javascript challange code

---
+++ JavaScript_challange/To do list/eventListener.js
@@ -0,0 +1,19 @@
+const items = [];
+const itemsList = document.querySelector(".items");
+
+document.querySelector("form#addItem").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const input = new FormData(e.target);
+  if (!input.get("item")) return;
+  const item = input.get("item").trim();
+  if (items.includes(item)) {
+    alert("dupilicate item");
+    return;
+  }
+  items.push(item);
+  itemsList.append(
+    Object.assign(document.createElement("li"), {
+      textContent: item
+    })
+  );
+});
