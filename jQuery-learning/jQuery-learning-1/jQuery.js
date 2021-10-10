1e27750e0efd4704628b5911f85360feacdb39ca
muhmp <defcoolz@gmail.com>
2021-10-10 18:49:35 +0900

    added jquery

---
+++ jQuery-learning/jQuery-learning-1/jQuery.js
@@ -0,0 +1,9 @@
+$("document").ready(function(){
+    $("h1").css("border", "2px solid black");
+    $("button").click(function(){
+       $("p").hide();
+
+       //apply heading style to h1 applied when click button clicked
+  
+   });
+});
