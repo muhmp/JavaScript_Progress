7b467fb814610354f89d7a11a560e296b80c6bbf
muhmp <defcoolz@gmail.com>
2021-10-30 23:59:51 +0900

    updates

---
+++ Object Oriented Programming/5.defineConstructor.js
@@ -0,0 +1,11 @@
+function Dog() {
+    this.name = "Albert";
+    this.color = "blue";
+    this.numLegs = 4;
+  }
+
+/**
+ * number 6
+ */
+//create a constructor
+let hound = new Dog();
---
+++ Object Oriented Programming/7.Argument.js
@@ -0,0 +1,14 @@
+
+/**
+ * 
+ * @param {*} name 
+ * @param {*} color
+ * Using argument 
+ */
+function Dog(name, color) {
+    this.name = name;
+    this.color = color;
+    this.numLegs = 4;
+  }
+  
+  let terrier = new Dog();
