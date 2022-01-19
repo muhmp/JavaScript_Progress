3e86c86f64a050b4f81d92d00209fe759e20c538
muhmp <defcoolz@gmail.com>
2022-01-20 07:44:40 +0900

    Create 2.diffArray.js

---
+++ Intermediate Algorithm Scripting/2.diffArray.js
@@ -0,0 +1,41 @@
+/**
+ * Diff two arrays
+ * Check two arrays and return a new array that contains only the items that are not in either of the original arrays.
+ */
+
+/* Solution 1: two functions
+    - indexOf
+    - swapthe array
+
+ function diffArray(arr1, arr2) {
+    var newArr = [];
+  
+    function onlyInFirst(first, second) {
+      // Looping through an array to find elements that don't exist in another array
+      for (var i = 0; i < first.length; i++) {
+        if (second.indexOf(first[i]) === -1) {
+          // Pushing the elements unique to first to newArr
+          newArr.push(first[i]);
+        }
+      }
+    }
+  
+    onlyInFirst(arr1, arr2);
+    onlyInFirst(arr2, arr1);
+  
+    return newArr;
+  }
+  
+  diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+  console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
+*/
+
+/* Solution 2: using concat, filter, includes method */
+
+function diffArray(arr1,arr2){
+    return arr1.concat(arr2).filter(item => !arr1.includes(item) || !arr2.includes(item));
+}
+
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+
+console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
