9e87de9ecf1ba02f0acab2a34e0eddf9ad5353fe
muhmp <defcoolz@gmail.com>
2022-02-07 18:16:44 +0900

    Create ceilMethod.js

---
+++ JavaScript_method/ceilMethod.js
@@ -0,0 +1,16 @@
+/** ceil method 
+ * The Math.ceil() function always rounds a number up to the next largest integer.
+*/
+
+
+console.log(Math.ceil(.95));
+// expected output: 1
+
+console.log(Math.ceil(4));
+// expected output: 4
+
+console.log(Math.ceil(7.004));
+// expected output: 8
+
+console.log(Math.ceil(-7.004));
+// expected output: -7
